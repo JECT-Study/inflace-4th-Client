cc50c3b69d312d5e2f12edeee1a1c09726f72b7f
juuhye <simjuhyee@gmail.com>
2026-04-02 23:34:51 +0900

    fix(onboarding) : youtube Icon 경로 수정
    src/features/onboarding/ui/OnboardingActionButtons.tsx -> 유튜브 아이콘 경로 수정

--- src/features/onboarding/ui/OnboardingActionButtons.tsx
+++ src/features/onboarding/ui/OnboardingActionButtons.tsx
@@ -1,6 +1,6 @@
 import { SocialLoginButton, usePopupOAuth } from '@/features/auth'
 import IconRightArrow from '@/shared/assets/rightwards-arrow-bold.svg'
-import YouTubeIcon from '@/features/auth/ui/social-login/assets/youtube-icon.svg'
+import YouTubeIcon from '@/shared/assets/youtube.svg'
 import { Button } from '@/shared/ui/button'
 import { useOnboardingModal } from '@/features/onboarding/model/useOnboardingModal'
 
@@ -13,30 +13,33 @@ export function OnboardingActionButtons() {
   })
   return (
     <>
-      {/* 구글 로그인 시 */}
-      {/* <div className='flex flex-col'>
-        <SocialLoginButton
-          icon={<YouTubeIcon />}
-          label={youtube.isLoading ? '로그인 중...' : 'Continue with YouTube'}
-          onClick={youtube.handleClick}
-          disabled={youtube.isLoading}
-        />
-        <button
-          className='mt-[var(--spacing-xs)] cursor-pointer text-center'
-          onClick={close}>
-          나중에 할래요
-        </button>
-      </div> */}
-      {/* 유튜브 연동 완료 시 */}
-      <Button
-        color={'primary'}
-        size={'lg'}
-        style={'filled'}
-        onClick={close}
-        rightIcon={<IconRightArrow />}
-        className='ml-auto'>
-        대시보드 둘러보기
-      </Button>
+      {/* 임시 div 태그 */}
+      <div className='flex flex-col'>
+        {/* 구글 로그인 시 */}
+        <div className='flex flex-col'>
+          <SocialLoginButton
+            icon={<YouTubeIcon />}
+            label={youtube.isLoading ? '로그인 중...' : 'Continue with YouTube'}
+            onClick={youtube.handleClick}
+            disabled={youtube.isLoading}
+          />
+          <button
+            className='mt-[var(--spacing-xs)] cursor-pointer text-center'
+            onClick={close}>
+            나중에 할래요
+          </button>
+        </div>
+        {/* 유튜브 연동 완료 시 */}
+        <Button
+          color={'primary'}
+          size={'lg'}
+          style={'filled'}
+          onClick={close}
+          rightIcon={<IconRightArrow />}
+          className='ml-auto'>
+          대시보드 둘러보기
+        </Button>
+      </div>
     </>
   )
 }
