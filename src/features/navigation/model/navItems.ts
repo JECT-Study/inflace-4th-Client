63e2aed68e1c9798be97973a916a511ce0549505
juuhye <simjuhyee@gmail.com>
2026-06-01 20:00:23 +0900

    feat(QA1-20): QA1-20, QA1-20 수정

--- src/features/navigation/model/navItems.ts
+++ src/features/navigation/model/navItems.ts
@@ -39,39 +39,41 @@ export const NAV_ITEMS: NavGroup[] = [
       },
     ],
   },
-  {
-    id: 3,
-    group: '비즈니스 전략',
-    items: [
-      {
-        title: '콘텐츠 전략',
-        icon: 'resing',
-        url: '',
-        requiredPlan: 'GROWTH',
-      },
-      {
-        title: '트렌드 매거진',
-        icon: 'article',
-        url: '',
-        requiredPlan: 'GROWTH',
-      },
-    ],
-  },
-  {
-    id: 4,
-    group: '협업',
-    items: [
-      {
-        title: '협업 매칭',
-        icon: 'message',
-        url: '',
-        requiredPlan: 'GROWTH',
-      },
-    ],
-  },
-  {
-    id: 5,
-    group: '지원',
-    items: [{ title: '고객센터', icon: 'question', url: '' }],
-  },
+  // 미사용 사이드 바 메뉴 임시 주석 처리
+
+  // {
+  //   id: 3,
+  //   group: '비즈니스 전략',
+  //   items: [
+  //     {
+  //       title: '콘텐츠 전략',
+  //       icon: 'resing',
+  //       url: '',
+  //       requiredPlan: 'GROWTH',
+  //     },
+  //     {
+  //       title: '트렌드 매거진',
+  //       icon: 'article',
+  //       url: '',
+  //       requiredPlan: 'GROWTH',
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 4,
+  //   group: '협업',
+  //   items: [
+  //     {
+  //       title: '협업 매칭',
+  //       icon: 'message',
+  //       url: '',
+  //       requiredPlan: 'GROWTH',
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 5,
+  //   group: '지원',
+  //   items: [{ title: '고객센터', icon: 'question', url: '' }],
+  // },
 ]
