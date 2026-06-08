848f3e29b37e1f037db3e812dcb704f23c4354a4
kim__yun_h <ajsl9749@naver.com>
2026-06-08 17:29:16 +0900

    Update page.tsx

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { LoginPage } from '@/pages/login'
 
-const title = '마이페이지 | 인플레이스'
+const title = '로그인 | 인플레이스'
 const description =
   '데이터 기반으로 임팩트 있는 유튜브 인플루언서를 찾고, 내 채널을 성장시키세요. 브랜드와 크리에이터를 연결하는 유튜브 마케팅 플랫폼, 인플레이스.'
 
